fa838e14fca9f5db98dd00065df7d137a74ab693
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2024-10-01 16:18:28 +0200

    Enable bulk-memory for all benchmarks

--- perf-deflate.sh
+++ perf-deflate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-RUSTFLAGS="-Ctarget-feature=+simd128" cargo build --release --target wasm32-wasi
+RUSTFLAGS="-Ctarget-feature=+simd128,+bulk-memory" cargo build --release --target wasm32-wasi
 wasmtime compile target/wasm32-wasi/release/wasm-zlib-benchmark.wasm
 perf record -k mono wasmtime --profile=jitdump --allow-precompiled wasm-zlib-benchmark.cwasm deflate zlib-rs
 perf inject --jit --input perf.data --output perf.jit.data
