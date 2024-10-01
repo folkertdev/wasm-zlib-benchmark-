a237e57d74add54fac32c7102b787576d3aa757d
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2024-10-01 16:42:53 +0200

    Compare against native

--- bench-inflate.sh
+++ bench-inflate.sh
@@ -2,5 +2,15 @@
 set -eu
 
 RUSTFLAGS="-Ctarget-feature=+simd128,+bulk-memory" cargo build --release --target wasm32-wasi
+
+# Use sse4.2 only. Wasm doesn't have 256bit simd, so using only sse2 is the fairest option.
+# This still has crc32 acceleration, so it is not entirely fair.
+RUSTFLAGS="-Ctarget-feature=+sse4.2" cargo build --release
+cp target/release/wasm-zlib-benchmark native-zlib-benchmark-sse42
+
+# Also check against AVX2
+RUSTFLAGS="-Ctarget-feature=+avx2" cargo build --release
+cp target/release/wasm-zlib-benchmark native-zlib-benchmark-avx2
+
 wasmtime compile target/wasm32-wasi/release/wasm-zlib-benchmark.wasm
-poop "wasmtime run --allow-precompiled wasm-zlib-benchmark.cwasm inflate miniz_oxide" "wasmtime run --allow-precompiled wasm-zlib-benchmark.cwasm inflate zlib-rs"
+poop "./native-zlib-benchmark-sse42 inflate zlib-rs" "./native-zlib-benchmark-avx2 inflate zlib-rs" "wasmtime run --allow-precompiled wasm-zlib-benchmark.cwasm inflate miniz_oxide" "wasmtime run --allow-precompiled wasm-zlib-benchmark.cwasm inflate zlib-rs"
